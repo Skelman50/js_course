5cb5f579c482b09781ee261cf91d45bf4698f833
Skelman50 <skelman50@gmail.com>
2018-09-15 20:37:49 +0300

    lesson_9

---
+++ lesson_9/index.js
@@ -0,0 +1,17 @@
+'use strict'
+console.log("Бублий Игорь");
+const a = 12;
+const b = 10;
+
+a > b ? console.log(a): console.log(b);
+a < b ? console.log(a): console.log(b);
+console.log(a + b);
+console.log(a % b);
+
+if (a > b + 10) {
+  console.log (a + 10);
+}else if (a < b) {
+  console.log(9);
+}else {
+  console.log(b);
+}
