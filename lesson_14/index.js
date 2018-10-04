648d6fc04ef0073e71f8621fc09bb419871108fc
Skelman50 <skelman50@gmail.com>
2018-10-04 19:58:41 +0300

    add 14 lesson

---
+++ lesson_14/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+  function init (arr,n) {
+	let ul = document.createElement('ul');
+    document.body.append(ul);
+
+	for (let i = 0; i < arr.length; i++) {
+	let className;
+	let content;
+	let atributes = {};
+		for(let keys in arr[i]) {
+			if (keys === "className") {		
+				className = arr[i][keys];
+			}else if (keys === "content") {
+				content = arr[i][keys];
+			}if (keys === "attributes") {
+				for (let key in arr[i][keys]) {
+					atributes[key] = arr[i][keys][key]
+				}
+			}
+		}
+
+	  for(let j = 0; j < n; j++) {
+	  	 let li = document.createElement('li');
+	  	 if (/*typeof(className) === 'string' &&*/ className != undefined) {
+	  	 li.className = className;
+	  	}
+	  	if (content != undefined) {
+	  	 li.innerText = content;
+	  	}
+	  	for (let keys in atributes) {
+	  		li.setAttribute(keys, atributes[keys])
+	  	}
+	  	 ul.append(li);
+	    }
+	}
+}
+
+function listen(){
+	let el = document.getElementsByTagName('ul');
+	let elem = el[0];
+	if (elem) {
+	elem.remove();
+	return true;
+}
+return false;	
+}
+
+let button = document.getElementsByTagName('button');
+let click = button[0];
+click.addEventListener('click',listen);
