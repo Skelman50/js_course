39d1dbd43661988e4e4ade22d09d164343e9a6b8
Skelman50 <skelman50@gmail.com>
2018-10-18 18:00:39 +0300

    add 18 lesson

---
+++ lesson_18/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+new Promise(function(resolve,reject){
+	setTimeout(function(){
+		resolve({isAuth:true})
+	},2000);
+})
+.then((authInfo)=>new Promise(function(resolve,reject){
+	if (!authInfo.isAuth) {
+		reject(null)
+	}else{
+	setTimeout(function(){
+		resolve({name:"Max"})
+	},2000);
+}
+}))
+.then((user)=>console.log(user.name))
+.catch((err)=>err);
+
+
+
