1793742c185d188204ce9be29fc8e986df8ee4c7
Skelman50 <skelman50@gmail.com>
2018-10-25 23:45:16 +0300

    better preloader

--- lesson_20/dom.js
+++ lesson_20/dom.js
@@ -2,23 +2,16 @@
 export default class Dom {
 	constructor(){
 		this.body = document.getElementsByTagName('body')[0];
-		this.preloader = document.createElement('div');
 		this.load = document.createElement('div');
+		this.body.append(this.load)
 		
 	}
 	showPreloader(){
 		
 			let this1 = this;
-			this.body.append(this.preloader)
-			this.preloader.style.fontSize = "5px"
-			this.preloader.append(this.load)
-			this.preloader.style.width="50px";
-			this.preloader.style.height="50px";
-			this.preloader.style.border="15px solid black"
-			this.preloader.style.borderRadius = '50%'
-			this.load.style.width="50px";
-			this.load.style.height="50px";
-			this.load.style.borderBottom="5px solid red"
+			this.load.style.width="100px";
+			this.load.style.height="100px";
+			this.load.style.background="url(spin.svg) no-repeat 50% 50%/cover"
 			this.load.style.borderRadius = '50%';
 			let sec = 0
 			this.id = setInterval(function(){
@@ -27,7 +20,7 @@ export default class Dom {
 			},50)
 	}
 	hidePreloader(){
-		this.preloader.remove()
+		this.load.remove()
 		
 	}
 	setCoordinates(coordinates) {
