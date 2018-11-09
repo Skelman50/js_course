93090e9ec160bc487fa209b385f2a7cf8943e83a
Skelman50 <skelman50@gmail.com>
2018-11-10 00:36:07 +0200

    add imitation

--- modul_3/index.js
+++ modul_3/index.js
@@ -8,14 +8,20 @@ const UIController = new MaskInput (document.querySelector('.js-input-selector')
   alwaysShowMask: true,
   maskChar: '_',
   onChange() {
+ 
+  	if (inputCard.value.match(/^(\d{4}-){3}\d{4}/) && name.value>1) {
+  		btn.disabled = false;
+  	}else{
+  		  btn.disabled = true;
+  	}
   	
-        if (inputCard.value.match(/^(\d{4}-){3}\d{4}/) && name.value.length >= 2) {
+        if (inputCard.value.match(/^(\d{4}-){3}\d{4}/)) {
             inputCard.style.outlineColor = "green";
-            btn.disabled = false;
+   
         } else {
-            inputCard.className = "invalid";
+       
             inputCard.style.outlineColor = "red";
-            btn.disabled = true;
+          
         }
 
   }
@@ -37,14 +43,27 @@ let getStr = JSON.parse(localStorage.getItem("table"));
 			table.append(tr)
 			let td1 = document.createElement('td')
 			tr.append(td1)
-			td1.innerText = key;
+			td1.innerText = key[0].toUpperCase() + key.slice(1);
 			let td2 = document.createElement('td')
 			tr.append(td2)
 			td2.innerText = getStr[key];
+			if (getStr[key]==="") {
+				td2.innerText = "Unknown"
+			}
 		}
+	if (localStorage.getItem("table")) {
+		let clear = document.createElement('button')
+		clear.innerText = 'очистить'
+		divResult.append(clear)
+		clear.addEventListener('click',function(){
+			table.innerText = "";
+			clear.remove()
+			localStorage.clear()
+		})
+	}
 
 name.addEventListener('input', function(){
-if (name.value.length >= 2 && inputCard.value.match(/^(\d{4}-){3}\d{4}/)) {
+if (name.value.length > 1 && inputCard.value.match(/^(\d{4}-){3}\d{4}/)) {
 	name.style.border = "1px solid grey";
 		name.style.outlineColor = "green";
 		btn.disabled = false;
@@ -58,10 +77,26 @@ if (name.value.length >= 2 && inputCard.value.match(/^(\d{4}-){3}\d{4}/)) {
 })
 
 btn.addEventListener('click', function(){
+	let r = document.getElementById('clear')
+	if (r!=null) {r.remove()}
+	
 	let result = UIController.input._visibleValue.split('-').join('')
 	table.innerText = 'LOADING...'
 	fetch (`https://api.bincodes.com/cc/?format=json&api_key=d96ca493f5be297f8c304a87edcdf6a8&cc=${result}`)
-	.then(res => res.json())
+	.then(res=> res = {
+ "bin": "515735",
+ "bank": "CITIBANK N.A.",
+ "card": "MASTERCARD",
+ "type": "CREDIT",
+ "level": "WORLD",
+ "country": "UNITED STATES",
+ "countrycode": "US",
+ "website": "",
+ "phone": "",
+ "valid": "true"
+}
+)
+	//.then(res => res.json())
 	.then(res => {
 		let lstrgSer = JSON.stringify(res)
 		localStorage.setItem("table", lstrgSer)
@@ -71,19 +106,27 @@ btn.addEventListener('click', function(){
 			table.append(tr)
 			let td1 = document.createElement('td')
 			tr.append(td1)
-			td1.innerText = key;
+			td1.innerText = key[0].toUpperCase() + key.slice(1);
 			let td2 = document.createElement('td')
 			tr.append(td2)
 			td2.innerText = res[key];
+			if (res[key]==="") {
+				td2.innerText = "Unknown"
+			}
 		}
+
+		
 		let clear = document.createElement('button')
+
 		clear.innerText = 'очистить'
+		clear.id = "clear"
 		divResult.append(clear)
 		clear.addEventListener('click',function(){
 			table.innerText = "";
 			clear.remove()
 			localStorage.clear()
 		})
+
 		
 		btn.disabled = false;
 		name.disabled = false;
--- modul_3/ui_controller.js
+++ modul_3/ui_controller.js
@@ -1,6 +1,32 @@
 
 import MaskInput from 'mask-input';
- 
- class UIController = new MaskInput;
 
- export default UIController;
+class UIController{
+
+}
+ 
+export default new UIController = new MaskInput (document.querySelector('.js-input-selector'), {
+  mask: '0000-0000-0000-0000',
+  alwaysShowMask: true,
+  maskChar: '_',
+  onChange() {
+  	if (inputCard.value.match(/^(\d{4}-){3}\d{4}/) && name.value>=2) {
+  		btn.disabled = false;
+  	}else{
+  		  btn.disabled = true;
+  	}
+  	
+        if (inputCard.value.match(/^(\d{4}-){3}\d{4}/)) {
+            inputCard.style.outlineColor = "green";
+   
+        } else {
+       
+            inputCard.style.outlineColor = "red";
+          
+        }
+
+  }
+});
+
+
+
