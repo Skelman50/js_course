2754360ab3416ae107a012b9987e33c4166c59c1
Skelman50 <skelman50@gmail.com>
2018-11-27 00:11:02 +0200

    correct rand to random

--- lesson_28/homeTask_additional/color.jsx
+++ lesson_28/homeTask_additional/color.jsx
@@ -7,15 +7,16 @@ export class Color extends React.Component {
 
     render(){
        let color = this.props.match.params.color;
-        if(color==="rand") {
+        if(color==="random") {
             let red = Math.round(-0.5 + Math.random() * 256);
             let green = Math.round(-0.5 + Math.random() * 256);
             let blue = Math.round(-0.5 + Math.random() * 256);
             color = `rgb(${red},${green},${blue})`
+            encodeURIComponent(color);
         }
         
         return(
-            <div style={{background:color,width:"100vw",height:"100vh",paddingTop:"50%"}}>
+            <div style={{background:decodeURIComponent(color),width:"100vw",height:"100vh",paddingTop:"50%"}}>
             
             </div>
         )
@@ -27,15 +28,16 @@ export class Text extends React.Component {
     render(){
         let text = this.props.match.params.text;
         let color = this.props.match.params.color;
-        if(color==="rand") {
+        if(color==="random") {
             let red = Math.round(-0.5 + Math.random() * 256);
             let green = Math.round(-0.5 + Math.random() * 256);
             let blue = Math.round(-0.5 + Math.random() * 256);
-            color = `rgb(${red},${green},${blue})`
+            color = `rgb(${red},${green},${blue})`;
+            encodeURIComponent(color);
         }
        
         return(
-           <div style={{background:color,width:"100vw",height:"100vh",paddingTop:"50%"}}>
+           <div style={{background:decodeURIComponent(color),width:"100vw",height:"100vh",paddingTop:"50%"}}>
            <h1 style={{textAlign:"center"}}>{text}</h1>
            </div>
         )
